b95c29cd5592418f43b34d0f2930f1cc79bf72bf
Sasho Stojkoski <112008646+Salewitcher@users.noreply.github.com>
2024-10-27 15:30:06 +0000

    Add homepage content

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn star_wardrobe.wsgi:application
--- star_wardrobe/star_wardrobe/settings.py
+++ star_wardrobe/star_wardrobe/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-8+@xb=+e$d=z7-e^1+uzq+2e_4w*2fg09dx#p88#)6658yhy3i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -105,12 +106,17 @@ WSGI_APPLICATION = 'star_wardrobe.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
