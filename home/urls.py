53bc9cb27cc23a1247d1d9704862a79bd042ec94
Sasho Stojkoski <112008646+Salewitcher@users.noreply.github.com>
2024-12-07 17:45:39 +0000

    Fix the products urls

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('products/', views.products, name='products'),
 ]
--- home/views.py
+++ home/views.py
@@ -6,7 +6,3 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
-
-def products(request):
-    
-    return render(request, 'products/products.html')
