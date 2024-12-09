d842b101e4a79b604f3253db58721f5972342b2c
Sasho Stojkoski <112008646+Salewitcher@users.noreply.github.com>
2024-12-09 10:16:21 +0000

    Update base css

--- star_wardrobe/settings.py
+++ star_wardrobe/settings.py
@@ -31,7 +31,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'star-wardrobe-1b58922e9a72.herokuapp.com',
