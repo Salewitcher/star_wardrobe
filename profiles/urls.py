e6b432c57dc2c1f73480f91e738620b6a3aacf64
Sasho Stojkoski <112008646+Salewitcher@users.noreply.github.com>
2025-02-21 09:52:58 +0000

    DEBUG = False

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('order_history/<order_number>', views.order_history, name='order_history'),
     path('newsletter-signup/', views.newsletter_signup, name='newsletter_signup'),
     path('newsletter-thank-you/', views.thank_you, name='thank_you'),
+    path('thank_you/', views.thank_you, name='thank_you'),
 ]
